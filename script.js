9aa7bbd3c8e2684e4a3cf028f6f07a675f389202
AnisaAisha <35626637+AnisaAisha@users.noreply.github.com>
2020-04-02 18:22:52 +0500

    added graph

---
+++ script.js
@@ -0,0 +1,46 @@
+
+window.onload = function () {
+	/*CanvasJS.addColorSet("shades", [//colorSet Array
+		"#91CCD9",
+		"#02735E",
+		"#03A678",
+		"#F2C46D",
+		"#F26B6B"]);*/
+	CanvasJS.addColorSet("shades", [//colorSet Array
+		"#CC2314",
+		"#F2561D",
+		"#F28729",
+		"#F2C53D",
+		"#F2E641"]);
+	
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	colorSet: "shades",
+	//backgroundColor: "#FCFFDB",
+	backgroundColor: "#f4f0ec",
+	
+	title:{
+		text:"Growth Rate"
+	},
+	axisX:{
+		interval: 1
+	},
+	axisY2:{
+		title: "COVID-19 Cases across different cities in Pakistan"
+	},
+	data: [{
+		type: "bar",
+		name: "companies",
+		axisYType: "secondary",
+		dataPoints: [
+			{ y: 20, label: "Sukkur" },
+			{ y: 7, label: "Karachi" },
+			{ y: 5, label: "Lahore" },
+			{ y: 9, label: "Quetta" },
+			{ y: 7, label: "Peshawar" }
+		]
+	}]
+});
+chart.render();
+
+}
